bff733069951959aa0a99eee1b9d6c4d8a8efcdd
Yikang Li <yikangl@student.unimelb.edu.au>
2025-03-18 00:35:41 +1100

    start coding for database accessor

--- src/main/java/io/github/yikangli2003/DatabaseAccessor.java
+++ src/main/java/io/github/yikangli2003/DatabaseAccessor.java
@@ -2,9 +2,26 @@ package io.github.yikangli2003;
 
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+
+import io.github.yikangli2003.entity.*;
+import org.mapdb.Serializer;
 
 public class DatabaseAccessor {
-    private static DB db = DBMaker.memoryDB().transactionEnable().make();
+    private static final DB database;
+    private static final HTreeMap<String, User> userMap;
+    private static final HTreeMap<String, DebtRecord> debtRecordMap;
+
+    static {
+        database = DBMaker.memoryDB().transactionEnable().make();
+        userMap = database.
+                hashMap("users").
+                keySerializer(Serializer.STRING).
+                valueSerializer(Serializer.JAVA).
+                createOrOpen();
+
+
+    }
 
     private static void initializeDataStructures() {
         // map and set
